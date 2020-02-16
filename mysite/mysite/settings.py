f76e5f1ce3592ca10b393d5e3cb210b4154d6d66
Gustavo Cruz <gu29gbi@gmail.com>
2020-02-16 09:48:05 -0300

    Modificado o time zone para utc-32

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC-32'
 
 USE_I18N = True
 
