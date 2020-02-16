daa2f25e52200959279ae7b4208adbf0b8428b12
Gustavo Cruz <gu29gbi@gmail.com>
2020-02-16 10:44:07 -0300

    Modificado o UTC que não estava funcionado e mudado a linguagem para pt-br

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC-32'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
